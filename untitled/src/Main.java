b890efc8605fdca44e50256a2e4f127f31eefb00
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-05-21 08:33:03 -0600

    Programa que saca el promedio de 3 numeros

---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String x = JOptionPane.showInputDialog("Ingresa el primer número:");
+        String y = JOptionPane.showInputDialog("Ingresa el segundo numero:");
+        String z = JOptionPane.showInputDialog("Ingresa el tercer numero:");
+        System.out.println("Resultado: "+((Double.parseDouble(x)+Double.parseDouble(y)+Double.parseDouble(z)))/3);
+
+    }
+}
